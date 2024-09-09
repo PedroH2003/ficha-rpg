337794c65e4d2a7ee794f4216823cfa2a8d1f1c6
PedroH2003 <pedrohenriquecandidosilva14@gmail.com>
2024-09-09 18:54:07 -0300

    Initial commit

---
+++ src/main/java/com/candido/fichaordem/FichaordemApplication.java
@@ -0,0 +1,13 @@
+package com.candido.fichaordem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FichaordemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FichaordemApplication.class, args);
+	}
+
+}
